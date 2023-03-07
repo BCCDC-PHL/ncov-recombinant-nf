a2e624157d4a377fad0112a444ca682698b56c51
taranewman <tara.newman98@gmail.com>
2023-03-06 17:52:25 -0800

    create a linelist subset

--- bin/add_pipeline_version.py
+++ bin/add_pipeline_version.py
@@ -29,6 +29,24 @@ def main(args):
 		df_temp.to_csv(file, sep = "\t", index = False)
 
 
+	# create a subset of only the columns required for QC Summary file
+	linelist_tsv_file = 'linelists/linelist.tsv'
+
+
+	# read in linelist.tsv file
+	linelist = pd.read_csv(linelist_tsv_file, sep = "\t")
+	
+	# rename column names 
+	linelist.rename(columns = {'status':'recombinant_status', 'lineage':'recombinant_lineage', 'parents_clade':'recombinant_parents_clade', 'ncov-recombinant_version':'ncov_recombinant_version'}, inplace = True)
+
+	
+	# subset only required columns
+	linelist_subset = linelist[['strain', 'recombinant_status','recombinant_lineage','recombinant_parents_clade','ncov_recombinant_version']]
+
+	
+	linelist_subset.to_csv("linelist_qc_summary_subset.tsv")
+
+
 
 
 
